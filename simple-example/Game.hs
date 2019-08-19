8c65c67a5b5506036e8ef74e8f6241d1e615a143
Julian Götz <julian.goetz@stud.htwk-leipzig.de>
2019-08-19 21:16:38 +0200

    added switch to static ball when grounded

--- simple-example/Game.hs
+++ simple-example/Game.hs
@@ -27,7 +27,7 @@ fallingBall = proc () -> do
 -- first flattens outer ClockInfo Reader with runReaderS, adds DTime to fallingBall's Arrow input
 -- then runReaderS_ runs inner GameEnv Reader to pass in the GameSettings
 flattenFallingBall :: Monad m => MSF m (DTime, ()) Position
-flattenFallingBall = runReaderS_ (runReaderS fallingBall) gs
+flattenFallingBall = runReaderS_ (runReaderS fallingToStatic) gs
   where
     gs = GameSettings leftPlayer rightPlayer groundHeight
     leftPlayer = V2 100 300
@@ -38,3 +38,11 @@ hitGround :: Monad m => SF (GameEnv m) Position Bool
 hitGround = arrM $ \b -> (yOfV2 b <=) <$> (lift $ asks groundHeightSettings)
   where
     yOfV2 v = case v of V2 _ y -> y
+
+-- Converts sf to stream of events. Events hold values when sf returned true.
+edgeSF :: Monad m => SF m a Bool -> SF m a (Event a)
+edgeSF sf = (arr id &&& sf) >>> arr (\(x, bool) -> if bool then Event x else NoEvent)
+
+fallingToStatic :: Monad m => SF (GameEnv m) () Position
+fallingToStatic = switch (fallingBall >>> (arr id &&& edgeSF hitGround)) (\pos -> constM $ return pos)
+
