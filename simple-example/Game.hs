62e5f9a8111486d3dea3853ecabf2bde5847ad1a
Julian Götz <julian.goetz@stud.htwk-leipzig.de>
2019-08-22 08:39:32 +0200

    added bouncing ball

--- simple-example/Game.hs
+++ simple-example/Game.hs
@@ -4,35 +4,27 @@ import GameState
 
 import Control.Monad.Reader hiding (asks, ask)
 import Control.Monad.Trans.MSF.Reader
+import Data.Functor.Identity
 import FRP.BearRiver hiding ((^+^))
 import SDL.Vect
 
-
-
 gameSF :: Monad m => SF m a GameState
-gameSF = (constM ask) >>> arr (\dt -> (dt, ())) >>> flattenFallingBall >>> (arr $ GameState)
+gameSF = (constM ask) >>> arr (\dt -> (dt, ())) >>> runReaderS_ (runReaderS_ (runReaderS (movingBall v0)) gs) ps >>> (arr $ GameState)
+  where
+    v0 = V2 0 50
+    gs = GameSettings 100
+    ps = PlayerSettings (V2 100 300) (V2 0 200)
 
 gravity :: Double
 gravity = -40
 
-fallingBall :: Monad m => SF (GameEnv m) () Position
-fallingBall = proc () -> do
-  v <- integral -< gravity
-  dp <- integral -< v
-  pos1 <- arrM(\d -> ((v2DP d) +) <$> (lift $ asks leftBallSettings)) -< dp
+fallingBall :: Monad m => Velocity -> SF (PlayerEnv m) () Position
+fallingBall v0 = proc () -> do
+  dv <- integral -< gravity
+  v1 <- arr (\dv -> case v0 of V2 vx vy -> V2 vx (vy+dv)) -< dv
+  dp <- integral -< v1
+  pos1 <- arrM(\dp -> ((dp) +) <$> (lift $ asks playerPosition0)) -< dp
   returnA -< pos1
-    where
-      v2DP = V2 0
-
--- first flattens outer ClockInfo Reader with runReaderS, adds DTime to fallingBall's Arrow input
--- then runReaderS_ runs inner GameEnv Reader to pass in the GameSettings
-flattenFallingBall :: Monad m => MSF m (DTime, ()) Position
-flattenFallingBall = runReaderS_ (runReaderS fallingToStatic) gs
-  where
-    gs = GameSettings leftPlayer rightPlayer groundHeight
-    leftPlayer = V2 100 300
-    rightPlayer = V2 400 300
-    groundHeight = 100
 
 hitGround :: Monad m => SF (GameEnv m) Position Bool
 hitGround = arrM $ \b -> (yOfV2 b <=) <$> (lift $ asks groundHeightSettings)
@@ -43,6 +35,27 @@ hitGround = arrM $ \b -> (yOfV2 b <=) <$> (lift $ asks groundHeightSettings)
 edgeSF :: Monad m => SF m a Bool -> SF m a (Event a)
 edgeSF sf = (arr id &&& sf) >>> arr (\(x, bool) -> if bool then Event x else NoEvent)
 
-fallingToStatic :: Monad m => SF (GameEnv m) () Position
-fallingToStatic = switch (fallingBall >>> (arr id &&& edgeSF hitGround)) (\pos -> constM $ return pos)
+movingBall :: Monad m => Velocity -> SF (GameEnv (PlayerEnv m)) () Position
+movingBall v0 = switch ((liftTransSF $ fallingBall v0) >>> (arr id &&& edgeSF (hitGround))) bouncingBall
+
+liftFallingSF :: Monad m => Velocity -> MSF (ClockInfo (GameEnv (PlayerEnv m))) () Position
+liftFallingSF v0 = liftTransSF $ fallingBall v0
+
+-- flattens a ReaderT r1 to arrow input, gives rise to an outer Reader r2
+liftInner :: Monad m => MSF (ReaderT r1 m) a b -> MSF (ReaderT r2 m) (r1, a) b
+liftInner sf = readerS $ addInput $ runReaderS sf
+
+-- creates a ReaderT in an inner layer
+liftTransSF :: Monad m => MSF (ReaderT t1 m) a b -> MSF (ReaderT t1 (ReaderT t2 m)) a b
+liftTransSF sf = readerS $ liftInner sf
+
+-- send an additional argument into an arrow
+addInput :: Monad m => MSF m a b -> MSF m (c,a) b
+addInput sf = second sf >>> arr snd
+
+staticBall :: Monad m => Position -> SF (m) () Position
+staticBall p0 = constM $ return p0
+
+bouncingBall :: Monad m => Position -> SF (GameEnv (PlayerEnv m)) () Position
+bouncingBall _ = movingBall $ V2 0 200
 
