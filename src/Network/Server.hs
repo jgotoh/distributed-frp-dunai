600dd0522c3dc767dc44fe6a04f867b266b4c5fe
jgoetz <julian.goetz@stud.htwk-leipzig.de>
2019-10-02 14:08:39 +0200

    added Server state as list of connected clients

--- src/Network/Server.hs
+++ src/Network/Server.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Network.Server where
 
 import Network.Common
@@ -10,11 +12,16 @@ import Control.Distributed.Process.Extras.Internal.Types
 import qualified Control.Distributed.Process.ManagedProcess as MP
 import qualified Control.Distributed.Process.Node as Node
 import Control.Monad
+import GHC.Generics (Generic)
 import qualified Network.Socket as N
 import qualified Network.Transport.TCP as NT
 import qualified Network.Transport as T
+import Type.Reflection
 
-type Client = (String, T.EndPoint)
+
+data Client = Client Nickname (P.SendPort (StateUpdate Message))
+  deriving (Generic, Show, Typeable)
+
 type ServerState = [Client]
 
 launchServer :: N.HostName -> N.ServiceName -> String -> IO ()
@@ -36,30 +43,39 @@ launchServer ip port name = do
   return ()
 
 pongServerProcess :: P.Process ()
-pongServerProcess = MP.serve () (MP.statelessInit Time.NoDelay) serverProcessDef
+pongServerProcess = MP.serve () initHandler pongProcessDef
+  where
+    initHandler _ = return (MP.InitOk [] Time.NoDelay)
 
-serverProcessDef :: MP.ProcessDefinition ()
-serverProcessDef = MP.statelessProcess {
+pongProcessDef :: MP.ProcessDefinition ServerState
+pongProcessDef = MP.defaultProcess {
                     MP.apiHandlers = [
                         MP.handleCall_ callPong
-                        , MP.handleCall_ callJoinRequest
+                        , MP.handleCall handleJoinRequest
                         , MP.handleCast castPong
                                   ]
                     , MP.infoHandlers = [
                         MP.handleInfo logInfo
+                        , MP.handleInfo handleMonitorNotification
                                       ]
                     , MP.exitHandlers = [
                         MP.handleExit logExit
                                         ]
                     , MP.timeoutHandler = logTimeout
                     , MP.shutdownHandler = logShutdown
-                    , MP.unhandledMessagePolicy = MP.Terminate
+                    , MP.unhandledMessagePolicy = MP.Log
                     }
 
-callJoinRequest :: JoinRequest -> P.Process JoinRequestResult
-callJoinRequest msg = do
-  P.liftIO $ print $ "JoinRequest: " ++ (show msg)
-  return $ JoinRequestResult $ Right JoinAccepted
+
+-- TODO if decline if client with nickname already exists
+handleJoinRequest :: MP.CallHandler ServerState JoinRequest JoinRequestResult
+handleJoinRequest s (JoinRequest nick port) = do
+  P.liftIO $ print $ "JoinRequest:: " ++ (show s')
+  _ <- P.monitorPort port
+  MP.reply (JoinRequestResult $ Right JoinAccepted) s'
+  where
+    client = Client nick port
+    s' = client:s
 
 callPong :: Message -> P.Process Message
 callPong x = do
@@ -70,6 +86,20 @@ castPong :: s -> Message -> MP.Action s
 castPong s x = do
   P.liftIO $ print $ "server: " ++ (show x)
   MP.continue s
+
+handleMonitorNotification :: MP.ActionHandler ServerState P.PortMonitorNotification
+handleMonitorNotification s (P.PortMonitorNotification ref port reason) = do
+  P.liftIO $ print $ "client process died: " ++ (show port) ++ " reason: " ++ (show reason)
+  P.liftIO $ print $ "new state: " ++ (show s')
+  P.unmonitor ref
+  MP.continue s'
+  where s' = removeClient port s
+
+removeClient :: P.SendPortId -> ServerState -> ServerState
+removeClient portId state = filter (hasPortId portId) state
+  where
+    hasPortId :: P.SendPortId -> Client -> Bool
+    hasPortId id' (Client _ port) = (P.sendPortId port) /= id'
 
 logInfo :: s -> Message -> MP.Action s
 logInfo s msg = do
